c4320b79d7253ba4904d201a813351269d379340
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-01-25 21:35:15 -0300

    Requisitando e formatando dados das imagens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,22 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    ({ pageParam = null }) => {
+      return api.get('/api/images', {
+        params: {
+          after: pageParam
+        }
+      })
+    },
+    {
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage.data.after ?? null
+      }
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages[0].data.data
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
@@ -36,7 +45,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        {/* <CardList cards={formattedData} /> */}
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
