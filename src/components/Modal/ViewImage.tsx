35e90cf4c54e9cc17feb3ccd302369561c15405a
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-01-26 17:10:07 -0300

    Visualizando imagem

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import {
   Modal,
   ModalOverlay,
@@ -6,6 +7,7 @@ import {
   ModalBody,
   Image,
   Link,
+  Skeleton,
 } from '@chakra-ui/react';
 
 interface ModalViewImageProps {
@@ -19,5 +21,53 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  const [isLoading, setIsLoading] = useState(true);
+
+  const initialRef = useRef()
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      initialFocusRef={initialRef}
+    >
+      <ModalOverlay />
+
+      <ModalContent
+        bgColor="pGray.800"
+        w="auto"
+        maxW="900px"
+      >
+        <ModalBody
+          p={0}
+        >
+          <Skeleton
+            isLoaded={!isLoading}
+          >
+            <Image
+              src={imgUrl}
+              maxW="900px"
+              maxH="600px"
+              onLoad={() => setIsLoading(false)}
+              ref={initialRef}
+            />
+          </Skeleton>
+        </ModalBody>
+        <ModalFooter
+          justifyContent="flex-start"
+          pl="0.625rem"
+          py="0.5rem"
+        >
+          <Link
+            href={imgUrl}
+            target="_blank"
+            rel="external"
+            fontSize={14}
+          >
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
